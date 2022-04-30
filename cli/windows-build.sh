abcbb832c1c6599d61b67f4f96f4084923c64a56
Vincenzo <vincylongo98@gmail.com>
2022-04-30 02:36:20 +0200

    fixed windows compiling

--- cli/windows-build.sh
+++ cli/windows-build.sh
@@ -7,7 +7,8 @@ fi
 g++ -std=c++17 -o ../build/main \
 *.cpp \
 ../headers/cpp-base64/base64.cpp \
-../headers/clip/clip.cpp ../headers/clip/image.cpp \
+../headers/clip/clip.cpp ../headers/clip/clip_win.cpp ../headers/clip/image.cpp \
+-lole32 -lshlwapi -lwindowscodecs \
 -lcryptopp \
 
 ret=$?
