91171d28d0566212155c4397960f4bfa4d898c84
Vincenzo <vincylongo98@gmail.com>
2022-05-04 01:53:31 +0200

    indentation fixed

--- headers/colors.h
+++ headers/colors.h
@@ -6,11 +6,7 @@ namespace color {
 
 #define RESET "\x1b[0m"
 
-std::string yellow(std::string str) {
-    return "\x1b[33m" + str + RESET;
-}
-std::string cyan(std::string str) {
-    return "\x1b[36m" + str + RESET;
-}
+std::string yellow(std::string str) { return "\x1b[33m" + str + RESET; }
+std::string cyan(std::string str) { return "\x1b[36m" + str + RESET; }
 
-}
+}  // namespace color
