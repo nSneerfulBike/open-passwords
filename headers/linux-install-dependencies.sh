7f058e96a641b1285031445a71d9cbb477c5e0fe
Vincenzo <vincylongo98@gmail.com>
2022-05-07 18:26:05 +0200

    added dependency to use g++ command

--- headers/linux-install-dependencies.sh
+++ headers/linux-install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y libcrypto++-dev libx11-dev libxcb1-dev libxcb-randr0 net-tools pkg-config
+sudo apt install -y libcrypto++-dev libx11-dev libxcb1-dev libxcb-randr0 net-tools pkg-config build-essential
 git clone https://github.com/nlohmann/json
 git clone https://github.com/dacap/clip.git
 git clone https://github.com/ReneNyffenegger/cpp-base64
